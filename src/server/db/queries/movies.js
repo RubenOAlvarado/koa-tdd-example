8f07abb54dd5ad31ee3b51c3b10599e35525668a
RubenOAlvarado <rubenosmaralvarado@gmail.com>
2021-07-01 14:07:21 -0500

    crud ready and all test passed

--- src/server/db/queries/movies.js
+++ src/server/db/queries/movies.js
@@ -12,7 +12,30 @@ function getSingleMovie(id){
         .where( { id: parseInt(id) } );
 }
 
+function addMovie(movie){
+    return knex('movies')
+    .insert(movie)
+    .returning('*');
+}
+
+function updateMovie(id, movie) {
+    return knex('movies')
+    .update(movie)
+    .where({ id: parseInt(id)})
+    .returning('*');
+}
+
+function deleteMovie(id){
+    return knex('movies')
+    .del()
+    .where({ id: parseInt(id) })
+    .returning('*');
+}
+
 module.exports = {
     getAllMovies,
-    getSingleMovie
+    getSingleMovie,
+    addMovie,
+    updateMovie,
+    deleteMovie
 };
--- src/server/routes/movies.js
+++ src/server/routes/movies.js
@@ -36,4 +36,79 @@ router.get(`${BASE_URL}/:id`, async (ctx) => {
     }
 });
 
+router.post(`${BASE_URL}`, async (ctx) => {
+    try {
+        const movie = await queries.addMovie(ctx.request.body);
+        if(movie.length){
+            ctx.status = 201;
+            ctx.body = {
+                status: 'success',
+                data: movie
+            };
+        }else{
+            ctx.status = 400;
+            ctx.body = {
+                status: 'error',
+                message: 'Something went wrong'
+            };
+        }
+    } catch (e) {
+        ctx.status = 400;
+        ctx.body = {
+            status: 'error',
+            message: e || 'Sorry, an error has ocurred'
+        };
+    }
+});
+
+router.put(`${BASE_URL}/:id`, async (ctx) => {
+    try {
+        const movie = await queries.updateMovie(ctx.params.id, ctx.request.body);
+        if(movie.length){
+            ctx.status = 200;
+            ctx.body = {
+                status: 'success',
+                data: movie
+            };
+        }else{
+            ctx.status = 404;
+            ctx.body = {
+                status: 'error',
+                message: 'That movie does not exist.'
+            };
+        }
+    } catch (e) {
+        ctx.status = 400;
+        ctx.body = {
+            status: 'error',
+            message: e.message || 'Sorry, an error has ocurred'
+        };
+    }
+});
+
+router.delete(`${BASE_URL}/:id`, async (ctx) => {
+    try {
+        const movie = await queries.deleteMovie(ctx.params.id);
+        if (movie.length) {
+            ctx.status = 200;
+            ctx.body = {
+                status: 'success',
+                data: movie
+            };
+        }else{
+            ctx.status = 404;
+            ctx.body = {
+                status: 'error',
+                message: 'That movie does not exist.'
+            };
+        }
+    } catch (e) {
+        ctx.status = 400;
+        ctx.body = {
+            status: 'error',
+            message: err.message || 'Sorry, an error has occurred.'
+        };
+    }
+});
+
 module.exports = router;
